127f775050ca3199ce6768a031ac4e540fbe5101
Mirrea <miriam_lande@hotmail.no>
2022-02-27 22:32:56 +0100

    state

--- staten/state_test.go
+++ staten/state_test.go
@@ -1,1 +1,14 @@
 package staten
+
+// Package for testing the represention of the state of the riverworld
+
+import "testing"
+
+// Test function implemented in line with the Golang's testing tool
+func TestViewState(t *testing.T) {
+	wanted := "[kylling rev korn hs ---\\ \\__/ _________________/---]"
+	state := ViewState()
+	if state != wanted {
+		t.Errorf("Feil, fikk %q, ønsket %q.", state, wanted)
+	}
+}
